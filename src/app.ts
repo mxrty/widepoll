c991ddcacaef6610701f5ad8ef1d6a4b53788c33
mxrty <Martynas67@gmail.com>
2021-04-11 18:20:31 +0100

    deployment 4

--- src/app.ts
+++ src/app.ts
@@ -20,7 +20,7 @@ app.use(cors());
 app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
+  app.use(express.static(path.join(__dirname, "..", "..", "client/build")));
 }
 
 //ROUTES
